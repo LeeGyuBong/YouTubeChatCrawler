16837b943c825ce66bedf0626ca47b2ba027abc6
이규봉 <killo1729@naver.com>
2021-08-29 04:09:06 +0900

    일부 코드 수정

--- YouTubeChatCrawler/YouTubeLib.cs
+++ YouTubeChatCrawler/YouTubeLib.cs
@@ -82,10 +82,11 @@ namespace YouTubeChatCrawler
 
 		public void Init()
 		{
-			var ret = Task.Run( async () =>
+			Task ret = Task.Run( async () =>
 			{
+				// OAuth 권한 셋팅
 				UserCredential credential;
-				using ( var stream = new FileStream( "client_secret.json", FileMode.Open, FileAccess.Read ) )
+				using ( FileStream stream = new FileStream( "client_secret.json", FileMode.Open, FileAccess.Read ) )
 				{
 					credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
 						GoogleClientSecrets.FromStream( stream ).Secrets,
@@ -94,6 +95,7 @@ namespace YouTubeChatCrawler
 						CancellationToken.None );
 				}
 
+				// YouTube Data API V3 권한 셋팅
 				youtubeService = new YouTubeService( new BaseClientService.Initializer
 				{
 					ApplicationName = "YouTube_Chat_Crawler",
@@ -108,7 +110,7 @@ namespace YouTubeChatCrawler
 		/// </summary>
 		public void GetComment( List<CommentInfo> commentList, string videoId, int no, string nextPageToken )
 		{
-			var request = youtubeService.CommentThreads.List( "snippet, replies" );
+			CommentThreadsResource.ListRequest request = youtubeService.CommentThreads.List( "snippet, replies" );
 			request.VideoId = videoId;
 			request.Order = CommentThreadsResource.ListRequest.OrderEnum.Relevance;
 			request.TextFormat = CommentThreadsResource.ListRequest.TextFormatEnum.PlainText;
@@ -117,47 +119,41 @@ namespace YouTubeChatCrawler
 
 			try
 			{
-				var response = request.Execute();
+				CommentThreadListResponse response = request.Execute();
+
 				foreach ( CommentThread item in response.Items )
 				{
-					try
+					CommentInfo info = new CommentInfo( no, item );
+					commentList.Add( info );
+
+					string parentId = item.Snippet.TopLevelComment.Id;
+
+					long? TotalReplyCount = item.Snippet.TotalReplyCount;
+					if ( TotalReplyCount > 0 )
 					{
-						CommentInfo info = new CommentInfo( no, item );
-						commentList.Add( info );
-
-						string parentId = item.Snippet.TopLevelComment.Id;
-
-						long? TotalReplyCount = item.Snippet.TotalReplyCount;
-						if ( TotalReplyCount > 0 )
+						// Part Replies가 가져오는 코멘트 수는 5개
+						// 5개가 넘으면 Comment.list를 통해 재검색하여 모든 데이터를 가져와야함
+						if ( TotalReplyCount > 5 )
 						{
-							// Part Replies가 가져오는 코멘트 수는 5개
-							// 5개가 넘으면 Comment.list를 통해 재검색하여 모든 데이터를 가져와야함
-							if ( TotalReplyCount > 5 )
-							{
-								List<CommentInfo> repliesList = new List<CommentInfo>();
-								GetReplyComment( repliesList, parentId, no, 1, null );
+							List<CommentInfo> repliesList = new List<CommentInfo>();
+							GetReplyComment( repliesList, parentId, no, 1, null );
 
-								repliesList.Sort( ( x, y ) => x.PublishedAt.CompareTo( y.PublishedAt ) );
-								commentList.AddRange( repliesList );
-							}
-							else
+							repliesList.Sort( ( x, y ) => x.PublishedAt.CompareTo( y.PublishedAt ) );
+							commentList.AddRange( repliesList );
+						}
+						else
+						{
+							int cno = 1;
+							for ( int i = item.Replies.Comments.Count; i > 0; --i )
 							{
-								int cno = 1;
-								for ( int i = item.Replies.Comments.Count; i > 0; --i )
-								{
-									CommentInfo replie = new CommentInfo( no, cno, item.Replies.Comments[ i - 1 ] );
-									commentList.Add( replie );
-									cno++;
-								}
+								CommentInfo replie = new CommentInfo( no, cno, item.Replies.Comments[ i - 1 ] );
+								commentList.Add( replie );
+								cno++;
 							}
 						}
+					}
 
-						no++;
-					}
-					catch ( Exception e )
-					{
-						Console.WriteLine( "[GetComment] Error!(Message:{0}, Stack:{1})", e.Message, e.StackTrace );
-					}
+					no++;
 				}
 
 				if ( response.NextPageToken != null )
@@ -174,13 +170,13 @@ namespace YouTubeChatCrawler
 		/// </summary>
 		private void GetReplyComment( List<CommentInfo> commentList, string parentId, int no, int cno, string nextPageToken )
 		{
-			var request = youtubeService.Comments.List( "snippet" );
+			CommentsResource.ListRequest request = youtubeService.Comments.List( "snippet" );
 			request.TextFormat = CommentsResource.ListRequest.TextFormatEnum.PlainText;
 			request.MaxResults = 100;
 			request.ParentId = parentId;
 			request.PageToken = nextPageToken;
 
-			var response = request.Execute();
+			CommentListResponse response = request.Execute();
 
 			cno = response.Items.Count;
 			foreach ( Comment item in response.Items )
@@ -207,12 +203,12 @@ namespace YouTubeChatCrawler
 		/// </summary>
 		public string GetliveChatID( string videoId )
 		{
-			var videoList = youtubeService.Videos.List( "LiveStreamingDetails" );
+			VideosResource.ListRequest videoList = youtubeService.Videos.List( "LiveStreamingDetails" );
 			videoList.Id = videoId;
 
-			var videoListResponse = videoList.Execute();
+			VideoListResponse videoListResponse = videoList.Execute();
 
-			foreach ( var item in videoListResponse.Items )
+			foreach ( Video item in videoListResponse.Items )
 			{
 				return item.LiveStreamingDetails.ActiveLiveChatId;
 			}
@@ -225,28 +221,36 @@ namespace YouTubeChatCrawler
 		/// </summary>
 		public async Task<string> GetLiveMessage( List<CommentInfo> commentList, string liveChatId, string nextPageToken )
 		{
-			var request = youtubeService.LiveChatMessages.List( liveChatId, "snippet,authorDetails" );
+			LiveChatMessagesResource.ListRequest request = youtubeService.LiveChatMessages.List( liveChatId, "snippet,authorDetails" );
 			request.PageToken = nextPageToken;
 			request.MaxResults = 2000;
 
-			var response = request.Execute();
-
-			foreach ( LiveChatMessage item in response.Items )
+			LiveChatMessageListResponse response = null;
+			try
 			{
-				try
+				response = request.Execute();
+
+				foreach ( LiveChatMessage item in response.Items )
 				{
 					CommentInfo info = new CommentInfo( item );
 					commentList.Add( info );
 				}
-				catch ( Exception e )
-				{
-					Console.WriteLine( "[GetLiveComment] Error!(Message:{0}, Stack:{1})", e.Message, e.StackTrace );
-				}
+
+				await Task.Delay( ( int )response.PollingIntervalMillis );
+			}
+			catch( Exception e )
+			{
+				// TODO : 상세한 예외처리
+				//forbidden( 403 ) forbidden 지정된 라이브 채팅에 대한 메시지를 검색하는 데 필요한 권한이 없습니다.
+				//forbidden( 403 ) liveChatDisabled 지정된 방송에 대해 라이브 채팅을 사용할 수 없습니다.
+				//forbidden( 403 ) liveChatEnded 지정된 라이브 채팅은 더 이상 라이브가 아닙니다.
+				//notFound( 404 )  liveChatNotFound 검색하려는 라이브 채팅을 찾을 수 없습니다.요청 liveChatId매개변수 의 값 이 올바른지 확인하	십시  오.
+				//rateLimitExceeded rateLimitExceeded   이전 요청 이후에 요청이 너무 빨리 전송되었습니다.이 오류는 메시지 검색을 위한 API 요청 이		YouTube의    새 로고침 빈도보다 더 자주 전송되어 불필요하게 대역폭을 낭비할 때 발생합니다.
+
+				Console.WriteLine( "[GetLiveComment] Error!(Message:{0}, Stack:{1})", e.Message, e.StackTrace );
 			}
 
-			await Task.Delay( ( int )response.PollingIntervalMillis );
-
-			return response.NextPageToken;
+			return response?.NextPageToken;
 		}
 	}
 }
